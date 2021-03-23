dac3b6c726136e87137dce1e3fe3a0ea71e8bbe3
DranoelProjects <leonard.wamberguepro@gmail.com>
2021-03-23 19:19:13 -0400

    deleting pre commit dependencies

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log("test2test");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
