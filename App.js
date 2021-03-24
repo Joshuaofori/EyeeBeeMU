869f6c1e0fd33889c1c136e9d34e5b1e54952d21
Loïc Vervisch <loic.vervisch@hei.yncrea.fr>
2021-03-24 17:15:10 -0400

    GET and POST are working

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://nodejs-express-app-cxlkb-2020-11-30.eu-gb.mybluemix.net/ai/session')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+  useEffect(() => {
+    fetch('https://nodejs-express-app-cxlkb-2020-11-30.eu-gb.mybluemix.net/ai', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        sessionId: data.response,
+        reqText: 'Palais Rameau',
+      }),
+    })
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {isLoading ? <Text>Loading...</Text>
+        : (
+          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center' }}>{data.response}</Text>
+        )}
       <StatusBar style={{ float: 'auto' }} />
     </View>
   );
