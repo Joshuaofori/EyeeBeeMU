456c6aee3ac512b8a8a96568ddacb6ffb2e8fd05
Loïc Vervisch <loic.vervisch@hei.yncrea.fr>
2021-03-24 10:40:29 -0400

    Test pre-commit 2

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log('bepbep');
+  console.log('bepbepbep');
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
