1d4555f6833618e283f6808ebe2d2ca1e4164331
Joshuaofori <oforijoshua37@gmail.com>
2021-03-25 13:03:14 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Algorithm from './algorithm/Algorithm';
 
 export default function App() {
-  console.log(Algorithm.converToBAse(336546));
-  console.log(Algorithm.covertToAscii('https://pasteboard.co/074 065 051 049 084 077 048 046 112 110 103/'));
+  // console.log(Algorithm.converToBAse(336546));
+  // console.log(Algorithm.covertToAscii('https://pasteboard.co/074 065 051 049 084 077 048 046 112 110 103/'));
 
   return (
     <View style={styles.container}>
