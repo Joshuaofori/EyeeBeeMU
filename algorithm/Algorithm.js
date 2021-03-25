48002f9a6a12edfe92402149ce11ea4b789435fc
Joshuaofori <oforijoshua37@gmail.com>
2021-03-25 10:28:46 +0100

    Create Algorithm.js

---
+++ algorithm/Algorithm.js
@@ -0,0 +1,7 @@
+/* eslint-disable camelcase */
+export default class Algorithm {
+  // eslint-disable-next-line class-methods-use-this
+  convert_to_base(number) {
+    return (number.toString(26));
+  }
+}
