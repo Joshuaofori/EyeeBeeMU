21a5262474d0b414f9c9efd88df173a814956ad0
Joshuaofori <oforijoshua37@gmail.com>
2021-03-25 12:59:14 +0100

    convert to base and regex

--- algorithm/Algorithm.js
+++ algorithm/Algorithm.js
@@ -1,7 +1,15 @@
-/* eslint-disable camelcase */
 export default class Algorithm {
-  // eslint-disable-next-line class-methods-use-this
-  convert_to_base(number) {
+  static converToBAse(pnumber) {
+    const number = pnumber;
     return (number.toString(26));
+  }
+
+  static covertToAscii(link) {
+    let word = '';
+    const numbers = link.replace(/\D/g, ' ').split(/(\s+)/).filter((entry) => /\S/.test(entry));
+    for (let i = 0; i < numbers.length; i += 1) {
+      word += String.fromCharCode(numbers[i]);
+    }
+    return word;
   }
 }
