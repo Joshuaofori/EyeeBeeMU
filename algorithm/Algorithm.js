f728de60dd5a2a836979df036c0e97e42f8e7a55
Joshuaofori <oforijoshua37@gmail.com>
2021-03-25 14:11:59 +0100

    Clue Y link and base conversion

--- algorithm/Algorithm.js
+++ algorithm/Algorithm.js
@@ -4,12 +4,18 @@ export default class Algorithm {
     return (number.toString(26));
   }
 
+  static replaceRange(s, start, end, substitute) {
+    return s.substring(0, start) + substitute + s.substring(end);
+  }
+
   static covertToAscii(link) {
     let word = '';
     const numbers = link.replace(/\D/g, ' ').split(/(\s+)/).filter((entry) => /\S/.test(entry));
     for (let i = 0; i < numbers.length; i += 1) {
       word += String.fromCharCode(numbers[i]);
     }
-    return word;
+    const first = link.indexOf(numbers[0]);
+    const last = link.indexOf(numbers[numbers.length - 1]) + numbers[numbers.length - 1].length;
+    return this.replaceRange(link, first, last, word);
   }
 }
