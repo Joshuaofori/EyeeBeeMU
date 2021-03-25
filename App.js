662d1717c53b953debf943bea2da6cfed21b7765
Loïc Vervisch <loic.vervisch@hei.yncrea.fr>
2021-03-25 19:36:16 -0400

    APIs are now in another file + DELETE

--- App.js
+++ App.js
@@ -1,40 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getSession, postMessage } from './API';
 
 export default function App() {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://nodejs-express-app-cxlkb-2020-11-30.eu-gb.mybluemix.net/ai/session')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-  useEffect(() => {
-    fetch('https://nodejs-express-app-cxlkb-2020-11-30.eu-gb.mybluemix.net/ai', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        sessionId: data.response,
-        reqText: 'Palais Rameau',
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
+  let isLoading = true;
+  const sessionId = getSession();
+  const botResponse = postMessage('hackaton', sessionId);
+  if (botResponse) {
+    isLoading = false;
+  }
   return (
     <View style={styles.container}>
       {isLoading ? <Text>Loading...</Text>
         : (
-          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center' }}>{data.response}</Text>
+          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center' }}>{botResponse}</Text>
         )}
       <StatusBar style={{ float: 'auto' }} />
     </View>
