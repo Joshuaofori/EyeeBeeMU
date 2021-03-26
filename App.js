fa320d3021d705d117b21d8d1dc440f05c6fac19
Duparcq <hakdup59@gmail.com>
2021-03-26 20:19:53 +0100

    ok

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -7,6 +7,11 @@ function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
+      <Image
+        source={require('./assets/point.png')}
+        resizeMode='contain'
+        style={{ width: 20, height: 20 }}
+      />
     </View>
   );
 }
@@ -19,6 +24,14 @@ function SettingsScreen() {
   );
 }
 
+function NotifyScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notify!</Text>
+    </View>
+  );
+}
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -27,6 +40,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Notify" component={NotifyScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
