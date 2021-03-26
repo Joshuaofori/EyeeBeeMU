953e838378ca03bb2161443a41e7fe784e947b09
Loïc Vervisch <loic.vervisch@hei.yncrea.fr>
2021-03-26 13:45:32 -0400

    Congratulations ;)

--- Components/SendItem.js
+++ Components/SendItem.js
@@ -3,25 +3,41 @@ import {
   View, TextInput, Button, StyleSheet,
 } from 'react-native';
 import PropTypes from 'prop-types';
+import ConfettiCannon from 'react-native-confetti-cannon';
 import { postMessage } from '../API';
 
 class SendItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasFound: false,
+    };
+  }
+
+  sendMessageAndGetResponse = (message, sessionId) => {
+    postMessage(sessionId, message).then((data) => {
+      console.log(data);
+      this.setState({ hasFound: false });
+      if (data === 'Toutes mes félicitations! Vous avez trouvé la solution! Vous êtes vraiment très forts. Quel est le nom de votre équipe pour enregistrer votre résultat?') {
+        this.setState({ hasFound: true });
+      }
+    });
+  };
+
   render() {
     const { sessionId } = this.props;
+    const { state } = this;
     return (
       <View style={styles.main_container}>
         <TextInput style={styles.message_container} placeholder="Your message..." />
-        <Button style={styles.send_button} title="Send" onPress={() => sendMessageAndGetResponse('Bonjour', sessionId)} />
+        <Button style={styles.send_button} title="Send" onPress={() => this.sendMessageAndGetResponse('Palais Rameau', sessionId)} />
+        {state.hasFound && (
+          <ConfettiCannon count={200} origin={{ x: -10, y: 0 }} />
+        )}
       </View>
     );
   }
 }
-
-const sendMessageAndGetResponse = (message, sessionId) => {
-  postMessage(sessionId, message).then((data) => {
-    console.log(data);
-  });
-};
 
 const styles = StyleSheet.create({
   main_container: {
