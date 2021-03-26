27ac39f85cb64039b6e836874fc71e214ddc56d5
DranoelProjects <leonard.wamberguepro@gmail.com>
2021-03-25 22:53:40 -0400

    initialing clues and passwordcheck components

---
+++ Components/Clues.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Clues extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Indice X : bep bep</Text>
+        <Text>Indice Y : bep bep</Text>
+        <Text>Indice Z : bep bep</Text>
+      </View>
+    );
+  }
+}
+
+export default Clues;
