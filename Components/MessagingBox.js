f95ee0dc8da0c00bc45a879ccbf2e07693ea7f7d
Bot <aquae.technology@gmail.com>
2021-03-26 21:31:30 -0400

    Message are now displaying properly and text input is getting cleared between messages

--- Components/MessagingBox.js
+++ Components/MessagingBox.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View, FlatList,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import MessageItem from './MessageItem';
 import SendItem from './SendItem';
-import messages from '../Helpers/messageData';
 
-class MessagingBox extends React.Component {
-  render() {
-    const { sessionId } = this.props;
-    return (
-      <View>
-        <FlatList
-          data={messages}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => <MessageItem message={item} />}
-        />
-        <SendItem sessionId={sessionId} />
-      </View>
-    );
-  }
-}
+const MessagingBox = (props) => {
+  const [messages, setMessages] = useState([]);
+  const { sessionId } = props;
+
+  const addMessage = (id, sender, content, secId, secSender, secContent) => {
+    const currentMessage = {
+      id,
+      sender,
+      content,
+      timestamp: ' 24/03/2021 à 19h48',
+    };
+    const botMessage = {
+      id: secId,
+      sender: secSender,
+      content: secContent,
+      timestamp: ' 24/03/2021 à 19h49',
+    };
+    const concat = [currentMessage, botMessage];
+    setMessages(messages.concat(concat));
+  };
+
+  return (
+    <View>
+      <FlatList
+        data={messages}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <MessageItem message={item} />}
+      />
+      <SendItem sessionId={sessionId} addMessage={addMessage} />
+    </View>
+  );
+};
 
 MessagingBox.propTypes = {
   sessionId: PropTypes.string.isRequired,
