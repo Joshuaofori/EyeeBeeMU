b1e13f61ae063a21ad9e770aa56a647f8f9dcf5b
Joshuaofori <oforijoshua37@gmail.com>
2021-03-28 15:58:33 +0200

    api

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/pinumber', function(req, res, next) {
+  var search = req.query.searchnext;
+  console.log(search);
   
   var readStream = fs.createReadStream('./files/pi_10_millions.txt','utf8');//{ start: 10000, end: 10178 }
   readStream.on('open',function(){
@@ -20,20 +22,16 @@ router.get('/pinumber', function(req, res, next) {
 var chunk;
 var found='';
   readStream.on('data',function(data){
-    if( data.match(('036695') )){
-    console.log("i found it");
+    if( data.match((search) )){
      chunk = data;
-    var index =chunk.indexOf('036695');
-    console.log(index);
+    var index =chunk.indexOf(search);
     found+= chunk.substring(index+6,index+13); 
    }
     else{
-      console.log("not here");
     }
    
   });
   readStream.on('end',function() {
-    console.log(found);
     const number = found.substring(0,7).replace(/\s/g, '');
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ value: number }));
