2f7dec62d4b5849ec2122aa046d264652b8ca15b
Duparcq <hakdup59@gmail.com>
2021-03-28 01:36:20 +0100

    ok

--- App.js
+++ App.js
@@ -1,23 +1,62 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MessagingBox from './Components/MessagingBox';
-import AppStatusBar from './Components/AppStatusBar';
 import Clues from './Components/Clues';
 import PasswordChecker from './Components/PasswordChecker';
 
-const THEME_COLOR = '#006699';
-const Tab = createBottomTabNavigator();
+const COLOR = '#FFFFFF';
+const Tab = createMaterialBottomTabNavigator();
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#000000" // couleur text cliqué black
+      inactiveColor="#3e2FFF"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: '# 000000' }} // ?
+      barStyle={{ backgroundColor: '#694fad', paddingBottom: 0 }} // couleur barre, remonter barre 0 pixels
+    >
+      <Tab.Screen
+        name="Feed"
+        component={MessagingBox}
+        options={{
+          tabBarColor: 'green',
+          tabBarLabel: 'Home',
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="cube" color={COLOR} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={PasswordChecker}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="bell" color={COLOR} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Clues}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="account" color={COLOR} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppStatusBar backgroundColor={THEME_COLOR} barStyle="light-content" />
-      <Tab.Navigator>
-        <Tab.Screen name="Messagerie" component={MessagingBox} />
-        <Tab.Screen name="IBM Enigme (à venir)" component={PasswordChecker} />
-        <Tab.Screen name="Notre Enigme" component={Clues} />
-      </Tab.Navigator>
+      <MyTabs />
     </NavigationContainer>
   );
 }
